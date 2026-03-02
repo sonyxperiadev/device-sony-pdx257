4244552979895c7e0bc0321b24758efe0410dbf3
Alin Jerpelea <alin.jerpelea@sony.com>
2026-03-02 10:05:17 +0100

    initial config for Xperia 10 VII (PDX257)
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

---
+++ device.mk
@@ -0,0 +1,96 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Device path
+DEVICE_PATH := device/sony/pdx257/rootdir
+
+DEVICE_PACKAGE_OVERLAYS += \
+    device/sony/pdx257/overlay
+
+# Device Specific Permissions
+PRODUCT_COPY_FILES := \
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
+
+# Audio Configuration
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/mixer_paths_parrot_qrd_sku1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_parrot_qrd_sku1.xml
+
+# Audio calibration
+PRODUCT_COPY_FILES +=
+
+# NFC Configuration
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
+# Touch firmware
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/firmware/focaltech_ts_fw_.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/focaltech_ts_fw_.bin
+
+# Audio calibration
+PRODUCT_COPY_FILES += \
+
+# Device-specific sensor config
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_default_sensors.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_default_sensors.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_dynamic_sensors.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_dynamic_sensors.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_power_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_power_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_qrd_ak0991x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_qrd_ak0991x_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_qrd_bmi26x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_qrd_bmi26x_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_qrd_dynamic_sensors.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_qrd_dynamic_sensors.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_qrd_hx32062secr.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_qrd_hx32062secr.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_qrd_lps22hh_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_qrd_lps22hh_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_qrd_lsm6dst_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_qrd_lsm6dst_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_qrd_stk3a5x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_qrd_stk3a5x_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_qrd_sx932x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_qrd_sx932x_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_qrd_tmd2725_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_qrd_tmd2725_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_qrdv2_lsm6dst_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_qrdv2_lsm6dst_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_sx932x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_sx932x_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_tmd2725_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_tmd2725_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_vertigo_ak991x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_vertigo_ak991x_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_vertigo_lsm6dst_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_vertigo_lsm6dst_0.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/parrot_vertigo_lsm6dst_1.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/parrot_vertigo_lsm6dst_1.json
+
+# Device Init
+PRODUCT_PACKAGES += \
+    fstab.pdx257 \
+    ramdisk-fstab.pdx257 \
+    init.recovery.pdx257 \
+    init.pdx257
+
+# Telephony Packages (AOSP)
+PRODUCT_PACKAGES += \
+    InCallUI \
+    Stk
+
+# SAR
+PRODUCT_PACKAGES += \
+    TransPowerSensors
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.sf.lcd_density=420 \
+    ro.usb.pid_suffix=20d
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/elbe/platform.mk)
+
+# copy NFC firmware
+$(call inherit-product-if-exists, vendor/nxp/nxp-vendor.mk)
+
+# include board vendor blobs
+$(call inherit-product-if-exists, vendor/sony/elbe-common/elbe-partial.mk)
