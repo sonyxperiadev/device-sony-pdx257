ab591a4076c12a4a436038c1a8da1b3a3e34e9c2
Pavel Dubrova <pashadubrova@gmail.com>
2026-08-06 13:30:15 +0300

    device: Add vendor prefix to USB PID suffix property
    The property is vendor-specific and must use a vendor prefix.
    The USB Gadget HAL expects the updated property.
    
    Also, update the suffix to '20e' to match the stock configuration.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- device.mk
+++ device.mk
@@ -89,7 +89,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.sf.lcd_density=420 \
-    ro.usb.pid_suffix=20d
+    ro.vendor.usb.pid_suffix=20e
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/elbe/platform.mk)
